13a2339c39ae799b41e9f49fec081bd4606568d8
ddale <ddale@yandex-team.ru>
2019-03-02 01:27:02 +0300

    build

---
+++ docs/precache-manifest.ffbbf0305399b172153a7b2f56c49d4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kv_peoplebook/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d1ebe2508d10e706a8cb",
+    "url": "/kv_peoplebook/static/js/runtime~main.d1ebe250.js"
+  },
+  {
+    "revision": "e6cd388508acce06af07",
+    "url": "/kv_peoplebook/static/js/main.e6cd3885.chunk.js"
+  },
+  {
+    "revision": "8ed95656164caa15abc9",
+    "url": "/kv_peoplebook/static/js/1.8ed95656.chunk.js"
+  },
+  {
+    "revision": "e6cd388508acce06af07",
+    "url": "/kv_peoplebook/static/css/main.4f5f5126.chunk.css"
+  },
+  {
+    "revision": "76f598661cbccc1d5bf33789a63be435",
+    "url": "/kv_peoplebook/index.html"
+  }
+];
